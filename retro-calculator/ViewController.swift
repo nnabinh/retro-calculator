dc90f8672562d4821f4e9015bdf1c7e7d9835947
Binh Nguyen <nnabinh@gmail.com>
2017-08-27 00:21:04 +0900

    Math & Logic handling

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -11,6 +11,19 @@ import AVFoundation
 
 class ViewController: UIViewController {
     var btnSound: AVAudioPlayer!
+    @IBOutlet weak var outputLbl: UILabel!
+    var runningNumber = ""
+    enum Operation: String {
+        case Divide = "/"
+        case Subtract = "-"
+        case Add = "+"
+        case Multiply = "*"
+        case Empty = "empty"
+    }
+    var currentOperation: Operation = .Empty
+    var leftValString = ""
+    var rightValString = ""
+    var result = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +37,33 @@ class ViewController: UIViewController {
         } catch let err as NSError {
             print(err.debugDescription)
         }
+        outputLbl.text = "0"
     }
     
     @IBAction func numberPressed(sender: UIButton) {
         playSound()
+        runningNumber += "\(sender.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
     }
     
     func playSound() {
@@ -35,6 +71,33 @@ class ViewController: UIViewController {
             btnSound.stop()
         }
         btnSound.play()
+    }
+    
+    func processOperation(operation: Operation) {
+        if currentOperation != .Empty {
+            if runningNumber != "" {
+                rightValString = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == .Add {
+                    result = "\(Double(leftValString)! + Double(rightValString)!)"
+                } else if currentOperation == .Divide {
+                    result = "\(Double(leftValString)! / Double(rightValString)!)"
+                } else if currentOperation == .Subtract {
+                    result = "\(Double(leftValString)! - Double(rightValString)!)"
+                } else if currentOperation == .Multiply {
+                    result = "\(Double(leftValString)! * Double(rightValString)!)"
+                }
+                
+                leftValString = result
+                outputLbl.text = result
+            }
+            currentOperation = operation
+        } else {
+            leftValString = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
     }
 }
 
